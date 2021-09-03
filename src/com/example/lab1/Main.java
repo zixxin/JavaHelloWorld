aa76cab7f7db9049c47e5eed5bae981bc391412d
윤지인 <74345861+22000487zixxin@users.noreply.github.com>
2021-09-03 15:59:23 +0900

    new changes

---
+++ src/com/example/lab1/Main.java
@@ -0,0 +1,13 @@
+package com.example.lab1;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello World!!!\n");
+		System.out.println("이름 입력 : ");
+		String name = s.next();
+		System.out.println("["+name+"]님 환영합니다~");
+	}
+}
